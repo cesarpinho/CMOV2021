e39055ed7331891c48f0de62f8740287962a4f1e
César Pinho <cesar98alexandre@gmail.com>
2020-10-25 23:21:19 +0000

    Create app project

---
+++ ACME/app/src/main/java/org/feup/cp/acme/Login.kt
@@ -0,0 +1,11 @@
+package org.feup.cp.acme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
