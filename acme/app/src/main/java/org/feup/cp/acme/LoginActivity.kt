62d0effec4d3476470233e9b7214f042d9846b04
César Pinho <cesar98alexandre@gmail.com>
2020-10-27 13:15:13 +0000

    Add transition between login and register page

--- acme/app/src/main/java/org/feup/cp/acme/LoginActivity.kt
+++ acme/app/src/main/java/org/feup/cp/acme/LoginActivity.kt
@@ -1,11 +1,20 @@
 package org.feup.cp.acme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewById<View>(R.id.btn_register_link).setOnClickListener(this::btnRegisterLink)
     }
+
+     private fun btnRegisterLink (view: View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
 }
