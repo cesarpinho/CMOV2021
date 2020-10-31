03c8a65d79bf57c46506489a8ad67cba7485fd58
César Pinho <cesar98alexandre@gmail.com>
2020-10-31 18:23:01 +0000

    Add home layout

--- acme/app/src/main/java/org/feup/cp/acme/LoginActivity.kt
+++ acme/app/src/main/java/org/feup/cp/acme/LoginActivity.kt
@@ -44,7 +44,7 @@ class LoginActivity : AppCompatActivity() {
 //            }
 //        })
 
-         val intent = Intent(this, RegisterActivity::class.java)
+         val intent = Intent(this, HomeActivity::class.java)
 
          startActivity(intent)
 
