3528bf99626faf116162866eeae5db4809cfe65f
César Pinho <cesar98alexandre@gmail.com>
2020-11-07 21:05:59 +0000

    Finish qrcode fragment

--- acme/app/src/main/java/org/feup/cp/acme/ui/tabs/cart/QRCodeFragment.kt
+++ acme/app/src/main/java/org/feup/cp/acme/ui/tabs/cart/QRCodeFragment.kt
@@ -4,20 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import org.feup.cp.acme.R
 
 class QRCodeFragment : Fragment() {
 
     /**
-     *
+     * Creates the QRCode tab view of the cart
      */
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_qrcode,container,false)
+        val view = inflater.inflate(R.layout.fragment_qrcode,container,false)
+
+        // TODO - Add QRcode to the view
+        view.findViewById<ImageView>(R.id.qrcode_image).setImageResource(R.drawable.ic_discount)
+
+        return view
     }
 
     /**
